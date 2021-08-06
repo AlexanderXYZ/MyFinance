14078d883fde501ed526757e719d545be200db09
Alexander <79529341506@yandex.ru>
2021-08-06 10:04:12 +0700

    setup ui operations and addOperation

--- app/src/main/java/com/buslaev/myfinance/ui/operations/OperationsFragment.kt
+++ app/src/main/java/com/buslaev/myfinance/ui/operations/OperationsFragment.kt
@@ -1,4 +1,4 @@
-package com.buslaev.myfinance.ui.operation
+package com.buslaev.myfinance.ui.operations
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,13 +8,13 @@ import android.view.ViewGroup
 import com.buslaev.myfinance.R
 
 
-class OperationFragment : Fragment() {
+class OperationsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_operation, container, false)
+        return inflater.inflate(R.layout.fragment_operations, container, false)
     }
 }
